6ef8ce6a194dcd848330cf4fb0f1d84b39737138
Bruno Henrique Paiva <bruno@sigane.com.br>
2020-10-20 17:49:08 -0300

    Fixed punishment model

--- src/database/models/PunishmentModel.ts
+++ src/database/models/PunishmentModel.ts
@@ -2,32 +2,38 @@ import {
   getModelForClass,
   modelOptions,
   prop,
-  Ref,
 } from '@typegoose/typegoose';
 import { Base } from '@typegoose/typegoose/lib/defaultClasses';
-import { ArchGuild } from './ArchGuildModel';
 import { Snowflake } from 'discord.js';
+
+export enum PunishmentType {
+  BAN,
+  MUTE,
+}
 
 @modelOptions({
   schemaOptions: {
     collection: 'punishments',
     timestamps: {
       createdAt: true,
+      updatedAt: false,
     },
   },
 })
 export class Punishment extends Base {
-  @prop({ required: true, ref: () => ArchGuild })
-  public guild!: Ref<ArchGuild>;
+  @prop({ required: true, type: () => String })
+  public guild!: Snowflake;
   @prop({ required: true, type: () => String })
   public punisher!: Snowflake;
   @prop({ required: true, type: () => String })
   public user!: Snowflake;
+  @prop({ required: true, enum: PunishmentType })
+  public type!: PunishmentType;
   @prop()
   public reason?: string;
   @prop()
   public createdAt?: Date;
-  @prop({ type: () => Number })
+  @prop({ type: () => Number, default: -1 })
   public expiresIn = -1;
 }
 
